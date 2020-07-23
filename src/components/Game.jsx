2f6afb13e64dc41f90ce80ded974deb3fc8e2c3e
Leonel Rosales <rleonel426@gmail.com>
2020-07-23 15:08:39 -0700

    start over button added

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import Board from './Board'
 import '../style.scss'
 
+
 class Game extends Component {
     state = {  
         height: 8,
         width: 8,
         mines: 10
     }
+
+
+startOver() {
+    window.location.reload(false);
+}
     render() { 
         const { height, width, mines } = this.state;
         return ( 
@@ -17,8 +23,11 @@ class Game extends Component {
                 width={width}
                 mines={mines}
                 />
-                <button >Start Over</button>
+                <button onClick={this.startOver} className="restart">Start Over</button>
             </div>
+            
+
+        
          );
     }
 }
